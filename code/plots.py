c54e81513e9e1687eb42b8ba8712ef3fd92dea15
fabio <lafebbra3@gmail.com>
2021-05-10 11:57:29 +0200

    fixed plotting

--- code/plots.py
+++ code/plots.py
@@ -8,6 +8,9 @@ base = os.path.abspath("")+'/results'
 if not os.path.exists(base ):
     os.mkdir(base)
 
+resumeT={}
+resumeY={}
+resumeX={}
 for dir in os.listdir(base):
     if not os.path.isfile(os.path.join(base, dir)):
         X={}
@@ -35,6 +38,9 @@ for dir in os.listdir(base):
                     flag = np.append(flag,float(flags))
                 y.close()
                 Y.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
+                if elem[-5] == '1':
+                    resumeY.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
+
             elif elem[0] == 'T':
                 flag=np.array([])
                 t=open(os.path.join(path,elem),'r')
@@ -46,6 +52,8 @@ for dir in os.listdir(base):
                     flag = np.append(flag,float(flags))
                 t.close()
                 T.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
+                if elem[-5] == '1':
+                    resumeT.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
             elif elem[0] == 'r':
                 flag=np.array([])
                 x=open(os.path.join(path,elem),'r')
@@ -57,17 +65,22 @@ for dir in os.listdir(base):
                     flag = np.append(flag,flags)
                 x.close()
                 X.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
+                if elem[-5] == '1':
+                    resumeX.update({elem[-9]+elem[-8]+elem[-7]+elem[-6]+elem[-5]:flag})
 
         fig, ax = plt.subplots(figsize=(10,5))
         for i in T.keys():
             listX = X.get(i)
             listT = T.get(i)
+            listY = Y.get(i)
             index = list(range(len(listX)))
             index.sort(key = listX.__getitem__)
             listX[:] = [listX[i] for i in index]
             listT[:] = [listT[i] for i in index]
-            ax.errorbar(x=listX, y=listT, color=(np.random.uniform(0,1),np.random.uniform(0,1),np.random.uniform(0,1)), label=i )
-        ax.set_ylim(bottom=0,top=int(lim))
+            listY[:] = [listY[i] for i in index]
+            ax.errorbar(x=listX, y=listT, color=(int(i[-5])*0.1,int(i[-2])*0.1,int(i[-1])*0.1),  label=i )
+        ax.set_ylim(bottom=0,top=int(lim)+500)
+        plt.title('Average over 100 runs -TIME-')
         plt.ylabel('steps')
         plt.xlabel('r')
         plt.legend()
@@ -77,17 +90,48 @@ for dir in os.listdir(base):
 
         fig, ax = plt.subplots(figsize=(10,5))
         for i in Y.keys():
-            listX = X.get(i)
-            listY = Y.get(i)
-            index = list(range(len(listX)))
-            index.sort(key = listX.__getitem__)
-            listX[:] = [listX[i] for i in index]
-            listY[:] = [listY[i] for i in index]
-            ax.errorbar(x=listX, y=listY, color=(np.random.uniform(0,1),np.random.uniform(0,1),np.random.uniform(0,1)), label=i )
+            ax.errorbar(x=X.get(i), y=Y.get(i), color=(int(i[-5])*0.1,int(i[-2])*0.1,int(i[-1])*0.1),  label=i )
         ax.set_ylim(bottom=0,top=1.1)
+        plt.title('Average over 100 runs -POP ON THE BEST NODE-')
         plt.ylabel('x1')
         plt.xlabel('r')
         plt.legend()
         plt.savefig(path+'/Results_POP.png')
         # plt.show()
         plt.close()
+
+fig, ax = plt.subplots(figsize=(10,5))
+for i in resumeT.keys():
+    listX = resumeX.get(i)
+    listT = resumeT.get(i)
+    index = list(range(len(listX)))
+    index.sort(key = listX.__getitem__)
+    listX[:] = [listX[i] for i in index]
+    listT[:] = [listT[i] for i in index]
+    ax.errorbar(x=listX, y=listT, color=(int(i[-2])*0.1,int(i[-1])*0.1,int(i[-5])*0.1), label='N='+i[-2] )
+ax.set_ylim(bottom=0,top=int(lim)+500)
+plt.title('Resuming plot for different tree size (N) -AVG TIME-')
+plt.ylabel('steps')
+plt.xlabel('r')
+plt.legend()
+plt.savefig(base+'/Results_Time.png')
+# plt.show()
+plt.close()
+
+fig, ax = plt.subplots(figsize=(10,5))
+for i in resumeY.keys():
+    listX = resumeX.get(i)
+    listY = resumeY.get(i)
+    index = list(range(len(listX)))
+    index.sort(key = listX.__getitem__)
+    listX[:] = [listX[i] for i in index]
+    listY[:] = [listY[i] for i in index]
+    ax.errorbar(x=listX, y=listY, color=(int(i[-2])*0.1,int(i[-1])*0.1,int(i[-5])*0.1),  label='N='+i[-2])
+ax.set_ylim(bottom=0,top=1.1)
+plt.title('Resuming plot for different tree size (N) -AVG POPULATION ON BEST NODE')
+plt.ylabel('x1')
+plt.xlabel('r')
+plt.legend()
+plt.savefig(base+'/Results_POP.png')
+# plt.show()
+plt.close()
